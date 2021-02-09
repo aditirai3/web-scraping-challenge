ee1f1623dc68de53313c02807ab79244ab0cf074
Aditi Rai <aditi45@gmail.com>
2021-02-09 16:32:46 -0500

    added jupyter notebook and updated scrape_nasa.py

--- Missions_to_Mars/scrape_nasa.py
+++ Missions_to_Mars/scrape_nasa.py
@@ -29,19 +29,45 @@ def scrape():
 
     
     # Visit the JPL url to get the image
-    image_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
-    browser.visit(image_url)
+    image_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/image/featured/mars1.jpg"
+    browser.visit(featured_image_url)
 
     # Scrape the image into Soup
     img_html = browser.html
     img_soup = BeautifulSoup(img_html, 'html.parser')
+        
+    mars_img = img_soup.find_all('img')
+    mars_img_path = mars_img.get('src')
+    featured_image_url = image_url +  mars_img_path
+    print(featured_image_url)
+
     
-    # Click FULL IMAGE link
-    browser.click_link_by_partial_text('FULL IMAGE')
-    
-    featured_image_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/image/featured/mars2.jpg"
-    soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
+    # Visit the Mars Facts site
+    marsfacts = 'https://space-facts.com/mars/'
+
+    # Read html and convert to dataframe
+    facts_table = pd.read_html(marsfacts)
+    df = facts_table[0]
+    df.columns = ['facts', 'value']
+    df.set_index('facts', inplace=True)
+    df
+
+    # Convert the data to an HTML table string
+    facts_html = df.to_html()
+    facts_html = facts_html.replace('\n', '')
+    facts_html
+
+    # Visit the Mars Hemispheres site
+        usgs_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+        browser.visit(usgs_url)
+
+        # Scrape page into Soup
+        html_usgs_mars = browser.html
+        soup_usgs_mars = BeautifulSoup(html_usgs_mars, 'html.parser')
+
+        # Get all Mars Hemisphere image attributes
+        mars_all_items = soup_usgs_mars.find_all('div', class_='item')
+        url_usgs = 'https://astrogeology.usgs.gov'
 
     # Store data in a dictionary
     costa_data = {
